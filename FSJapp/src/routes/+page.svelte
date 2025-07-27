819ea54b45ad24f923f31583b3496d16613f2aba
ImaniAN <imanix8@users.noreply.github.com>
2025-07-27 21:57:01 +0200

    Add footer section with dynamic year and gradient background; update component titles for clarity

--- FSJapp/src/routes/+page.svelte
+++ FSJapp/src/routes/+page.svelte
@@ -5,22 +5,22 @@
 <div class="h-max">
 	<div class="grid grid-cols-3 gap-2 p-2">
 		<div class="h-full rounded-lg bg-gray-300 col-span-2 flex items-center justify-center">
-			<span class="text-gray-600 font-mdium">Main Content</span>
+			<span class="text-gray-600 font-medium">Title Comp</span>
 		</div>
 
 		<div class="h-full rounded-lg bg-gray-100 p-1">
 			<div class="grid grid-cols-2 gap-2 mb-1 h-3/4">
 				<div class="h-full rounded bg-gray-300 flex items-center justify-center">
-					<span class="text-xs text-gray-600">Account</span>
+					<span class="text-xs text-gray-600">Account </span>
 				</div>
 				<div class="h-full rounded bg-gray-300 flex items-center justify-center">
-					<span class="text-xs text-gray-600">Settings</span>
+					<span class="text-xs text-gray-600">Settings Comp</span>
 				</div>
 			</div>
 
 			<!-- Progress Bar -->
 			<div class=" h-1/6 rounded bg-gray-300 flex items-center justify-center">
-				<span class="text-xs text-gray-600">Progress</span>
+				<span class="text-xs text-gray-600">Progress Comp</span>
 			</div>
 		</div>
 	</div>
@@ -28,20 +28,13 @@
 	<!-- Navigation Section -->
 	<div class="grid grid-cols-4 gap-2 p-2 h-72">
 		<div class="h-full rounded-lg bg-gray-300 flex items-center justify-center">
-			<span class="text-gray-600 font-medium">Left Nav</span>
+			<span class="text-gray-600 font-medium">Left Nav Comp</span>
 		</div>
 		<div class="h-full rounded-lg bg-gray-300 col-span-2 flex items-center justify-center">
-			<span class="text-gray-600 font-medium">Navigation Cards</span>
+			<span class="text-gray-600 font-medium">Navigation Cards Comp</span>
 		</div>
 		<div class="h-full rounded-lg bg-gray-300 flex items-center justify-center">
-			<span class="text-gray-600 font-medium text-center">Data Legend</span>
-		</div>
-	</div>
-
-	<!-- Footer Section -->
-	<div class="p-2">
-		<div class="h-full rounded-lg bg-gray-300 flex items-center justify-center">
-			<span class="text-gray-600 font-medium">Footer Content</span>
+			<span class="text-gray-600 font-medium text-center">Data Legend Comp</span>
 		</div>
 	</div>
 </div>
